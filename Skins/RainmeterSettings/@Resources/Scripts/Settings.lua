bf8b215358efd0194c31b520e9e4eb8885d5ee72
Caleb Heuer <caleb.heuer@gmail.com>
2019-01-27 20:20:54 -0700

    Documentation cleanup, add pivot and switch functions

--- Skins/RainmeterSettings/@Resources/Scripts/Settings.lua
+++ Skins/RainmeterSettings/@Resources/Scripts/Settings.lua
@@ -27,7 +27,7 @@
 -- --------------------------------------------------------------------------------
 -- Documentation: https://github.com/raiguard/rainmeter-settings/blob/master/README.md
 
-debug = true
+debug = false
 
 function Initialize()
 
@@ -44,8 +44,7 @@ end
 
 function Update() end
 
--- for use with toggle buttons. Toggles the specified variable between the two
--- given states
+-- oggles the specified variable between the two given states
 function Toggle(variable, onState, offState, actionSet, ifLogic, oSettingsPath, oConfigPath)
 
 	local value = SKIN:GetVariable(variable)
@@ -78,6 +77,29 @@ function Set(variable, input, actionSet, ifLogic, oSettingsPath, oConfigPath)
 	RmLog(variable .. '=' .. input, 'Debug')	
 	UpdateMeters()
 	ActionSet(actionSet, ifLogic, input)
+
+end
+
+-- changes variable to the next or previous value, based on a provided data table
+function Pivot(variable, data, direction, actionSet, ifLogic, oSettingsPath, oConfigPath)
+
+	local lSettingsPath = oSettingsPath or settingsPath
+	local lConfigPath = oConfigPath or configPath
+
+	local tableLength = table.length(data)
+	local index = table.find(data, SKIN:GetVariable(variable))
+	
+	if index < tableLength and direction == 'right' then
+		SetVariable(variable, data[index + 1], lSettingsPath, lConfigPath)
+		RmLog(variable .. '=' .. data[index + 1], 'Debug')	
+		UpdateMeters()
+		ActionSet(actionSet, ifLogic, data[index + 1])
+	elseif index > 1 and direction == 'left' then
+		SetVariable(variable, data[index - 1], lSettingsPath, lConfigPath)
+		RmLog(variable .. '=' .. data[index - 1], 'Debug')
+		UpdateMeters()	
+		ActionSet(actionSet, ifLogic, data[index - 1])
+	end
 
 end
 
@@ -144,6 +166,12 @@ end
 -- value both in the settings skin and the primary skin
 function SetVariable(name, parameter, filePath, configPath)
 
+	-- -- handle any escaped variables
+	-- while true do
+	-- 	local v = string.match(parameter, '%#%*(.*)%*%#')
+		
+
+	-- enact the changes within the skin
 	SKIN:Bang('!SetVariable', name, parameter)
 	if filePath == nil then SKIN:Bang('!WriteKeyValue', 'Variables', name, parameter) 
 		else SKIN:Bang('!WriteKeyValue', 'Variables', name, parameter, filePath) end
@@ -173,6 +201,32 @@ function table.length(T)
     local count = 0
     for _ in pairs(T) do count = count + 1 end
     return count
+end
+
+function table.find(t, value)
+	for _,v in pairs(t) do
+		if (v == value) then
+			return _
+		end
+	end
+	return false
+end
+
+printIndent = '     '
+
+-- prints the entire contents of a table to the Rainmeter log
+function PrintTable(table)
+    for k,v in pairs(table) do
+        if type(v) == 'table' then
+            local pI = printIndent
+            RmLog(printIndent .. tostring(k) .. ':')
+            printIndent = printIndent .. '  '
+            PrintTable(v)
+            printIndent = pI
+        else
+            RmLog(printIndent .. tostring(k) .. ': ' .. tostring(v))
+        end
+    end
 end
 
 -- --------------------------------------------------------------------------------
